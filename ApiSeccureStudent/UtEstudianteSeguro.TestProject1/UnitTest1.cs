1fb8cf1cdc978fcb7c7cd0c21bc834bc146b79c9
AnahiSamanthaAgredaCruz <a201911877@estudiantes.upsa.edu.bo>
2020-09-08 18:25:07 -0400

    Se incluyo elproyecto de UnitTest

---
+++ ApiSeccureStudent/UtEstudianteSeguro.TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using ApiSeccureStudent.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtEstudianteSeguro.TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetStudent()
+        {
+            //Arange
+            StudentsController studentsController = new StudentsController();
+
+
+            //Act
+
+            var ListaEstudiantes = studentsController.GetStudents();
+
+
+            //Assert
+            Assert.IsNotNull(ListaEstudiantes);
+        }
+    }
+} 
